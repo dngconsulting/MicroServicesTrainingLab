702b8632e4ca8a574840d1e865568ed8a79acedb
Sami <sami.jaber@gmail.com>
2019-06-18 16:03:17 +0200

    Exception Handling with RestControllerAdvice

--- src/main/java/com/dng/api/exception/CourseNotFoundException.java
+++ src/main/java/com/dng/api/exception/CourseNotFoundException.java
@@ -1,7 +1,15 @@
 package com.dng.api.exception;
 
+//@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class CourseNotFoundException extends RuntimeException {
+
+    private String errorCode = "COURSE_NOT_FOUND" ;
+
     public CourseNotFoundException(String id) {
         super("Le cours " + id + " n'existe pas ");
     }
+    public String getErrorCode() {
+        return errorCode;
+    }
+
 }
