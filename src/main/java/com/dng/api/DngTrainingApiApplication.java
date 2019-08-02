412657cbd84db72eb9d355892b383ab9bb7f7bb8
Sami <sami.jaber@gmail.com>
2019-08-02 18:43:32 +0200

    Fix endpoints and add metrics with grafana & prometheus

--- src/main/java/com/dng/api/DngTrainingApiApplication.java
+++ src/main/java/com/dng/api/DngTrainingApiApplication.java
@@ -1,8 +1,12 @@
 package com.dng.api;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -12,4 +16,11 @@ public class DngTrainingApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DngTrainingApiApplication.class, args);
 	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
+
 }
